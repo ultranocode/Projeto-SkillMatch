4b3dbf4fcb4b658e12aaf07260123a3a8909305d
ultranocode <ultra.nocode@gmail.com>
2026-05-22 15:26:22 -0300

    criação candidatos

---
+++ skillmatch.js
@@ -0,0 +1,50 @@
+//Canditatos 
+
+const candidato1 = {
+  nome: "Juliana",
+  area: "UX/UI Design",
+  habilidades: ["Figma", "Prototipação", "Design Responsivo", "Canva"],
+  experienciaMeses: 5
+};
+
+const candidato2 = {
+  nome: "Marcos",
+  area: "Full Stack",
+  habilidades: ["JavaScript", "React", "Node.js", "MongoDB"],
+  experienciaMeses: 12
+};
+
+const candidato3 = {
+  nome: "Bruno",
+  area: "Front-End",
+  habilidades: ["HTML", "CSS", "JavaScript", "React"],
+  experienciaMeses: 4
+};
+
+const candidato4 = {
+  nome: "Camila",
+  area: "Front-End",
+  habilidades: ["Vue.js", "Git", "Responsividade", "Bootstrap"],
+  experienciaMeses: 7
+};
+
+const candidato5 = {
+  nome: "Rafael",
+  area: "Front-End",
+  habilidades: ["JavaScript", "TypeScript", "React", "Figma"],
+  experienciaMeses: 10
+};
+
+const candidato6 = {
+  nome: "Larissa",
+  area: "Front-End",
+  habilidades: ["HTML", "CSS", "Sass", "Kanban"],
+  experienciaMeses: 5
+};
+
+const candidato7 = {
+  nome: "Eduardo",
+  area: "Front-End",
+  habilidades: ["Next.js", "Tailwind CSS", "GitHub", "API REST"],
+  experienciaMeses: 9
+};
